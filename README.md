088f3a888ad56a95b6cdaa9a0d0c066d6835240a
richielollis <richielollisdesktop@richies-mini.lan>
2022-08-15 23:40:26 -0400

    finish localStorage fucntionality and finished readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # work-day-scheduler
+
+Schedule workday by the hour
+
+## Description
+
+Work-Day-Scheduler is a website that allows users to create and save events that need to be completed during their workday hours. Each hour block will display either gray (hours past), red (current hour), or green (future hour). Users will see that their events are saved to each hour block even if they choose to reload the webpage.
+
+## GitHub
+
+https://github.com/richielollis/work-day-scheduler
+
+## Deployed Website
+
+https://richielollis.github.io/work-day-scheduler/
+
+## Screenshots
+
+![Alt text](./assets/images/Screen%20Shot%202022-08-15%20at%2011.33.21%20PM.png "Work-Day-Scheduler")
+
---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    // display current day in header
+    $('#currentDay').text(moment().format('dddd' + ', ' + 'MMMM Do'));
+
+    // add events from localStorage to corresponding .description
+    $(".description").each(function() {
+        $(this).val(localStorage.getItem($(this).parent().attr('id')));
+    });
+    
+    // checks currentHour against each hourBlock
+    var checkHour = function() {
+        currentHour = moment().format('HH');
+
+        $('.time-block').each(function() {
+            var hourBlock = parseInt($(this).attr('id').split('-')[1]); 
+
+            if (currentHour > hourBlock) {
+                $(`#hour-${hourBlock}`).children('.description').addClass('past');
+            } else if (currentHour < hourBlock) {
+                $(`#hour-${hourBlock}`).children('.description').removeClass('past');
+                $(`#hour-${hourBlock}`).children('.description').addClass('future');
+            } else {
+                $(`#hour-${hourBlock}`).children('.description').removeClass('past');
+                $(`#hour-${hourBlock}`).children('.description').removeClass('future');
+                $(`#hour-${hourBlock}`).children('.description').addClass('present');
+            }  
+        })
+    };
+
+    // run checkHour() every hour 
+    setInterval(checkHour(), 1000 * 60 * 60);
+   
+    // save events to localStorage 
+    $('.saveBtn').on('click', function() {
+        var event = $(this).siblings('.description').val();
+        localStorage.setItem($(this).parent().attr('id'), event);
+    });
+});
+
